a264f1deb1717fa43d26550025dc787c09d3cf94
xcode <xcode@c1r009n06.amm.vsu.ru>
2016-10-08 15:16:33 +0300

    misery sila

--- api-DESIGN/SingleEventTableViewController.swift
+++ api-DESIGN/SingleEventTableViewController.swift
@@ -60,7 +60,6 @@ class SingleEventTableViewController: UITableViewController {
         
         return cell
     }
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
